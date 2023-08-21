162f8e8c13623d35a6f630f86a1879e87331a797
Wasim Shaikh <wasim.shaikh@afourtech.com>
2023-08-21 14:21:49 +0530

    Adding hashmap

---
+++ collection/HashMapWorking.java
@@ -0,0 +1,15 @@
+package org.git.collection;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
+//refer the notes
+public class HashMapWorking {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+
+        HashMap<String,String> map = new HashMap<>(2, 1.0F);
+
+
+    }
+}
